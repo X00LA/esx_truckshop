484b5895f94711a45fdea79307be9d64131937ec
Markus Petautschnig <xoolalp@gmail.com>
2021-06-26 17:42:41 +0200

    Deleted most language files

--- config.lua
+++ config.lua
@@ -4,16 +4,16 @@ Config.MarkerColor                = {r = 120, g = 120, b = 240}
 Config.EnablePlayerManagement     = false -- enables the actual car dealer job. You'll need esx_addonaccount, esx_billing and esx_society
 Config.ResellPercentage           = 50
 
-Config.Locale                     = 'de'
+Config.Locale                     = 'en'
 
 Config.LicenseEnable = true -- require people to own drivers license when buying vehicles? Only applies if EnablePlayerManagement is disabled. Requires esx_license
 
 -- looks like this: 'LLL NNN'
 -- The maximum plate length is 8 chars (including spaces & symbols), don't go past it!
-Config.PlateLetters = 4
-Config.PlateNumbers = 4
-Config.PlateUseSpace = false
-Config.LegacyFuel = true
+Config.PlateLetters = 3
+Config.PlateNumbers = 3
+Config.PlateUseSpace = true
+Config.LegacyFuel = false
 
 Config.Zones = {
 
